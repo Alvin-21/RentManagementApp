e7c8e0822d9612c165d3fa3bacc4787c2f53b4f2
Alvin <alvinawuor8@gmail.com>
2025-11-10 13:06:19 +0300

    feat: Add logic for capturing data from first screen

--- app/src/main/java/com/example/rentmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/rentmanagementapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.rentmanagementapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,5 +20,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val firstNameInput = findViewById<EditText>(R.id.firstName)
+        val secondNameInput = findViewById<EditText>(R.id.secondName)
+        val rentAmountInput = findViewById<EditText>(R.id.rentAmount)
+        val displayRentButton = findViewById<Button>(R.id.displayRent)
+
+        displayRentButton.setOnClickListener {
+            val firstName = firstNameInput.text.toString()
+            val secondName = secondNameInput.text.toString()
+            val rentAmount = rentAmountInput.text.toString()
+        }
     }
 }
